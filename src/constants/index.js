d0d19f58b4f3fc1131124ac75b24c7c0da3ad43d
Yan Zhabin <iyanello@gmail.com>
2018-01-21 18:05:10 +0200

    Remove token (github will remove it in any case).

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,2 @@
-export const GITHUB_PERSONAL_ACCESS_TOKEN = '6adcabd2aca691a1ec650aa0ef3fdeaf664c947d'
+export const GITHUB_PERSONAL_ACCESS_TOKEN = ''
 export const GITHUB_API_URL = 'https://api.github.com/graphql'
